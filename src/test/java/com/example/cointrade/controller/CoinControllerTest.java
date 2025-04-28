8c3ac362d2591f2b5887fe9c0143ce3b1b79ac1f
JunSeobKim <wnstjq@gmail.com>
2025-04-29 00:05:42 +0900

    COIN-007 add coin search controller interface

---
+++ src/test/java/com/example/cointrade/controller/CoinControllerTest.java
@@ -0,0 +1,16 @@
+package com.example.cointrade.controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CoinControllerTest {
+
+    @Test
+    @Disabled
+    @DisplayName("should getCoins and return CoinResponseV1")
+    void shouldGetCoinsAndReturnCoinResponseV1() {
+        // TODO: coin-009
+        throw new UnsupportedOperationException();
+    }
+}
